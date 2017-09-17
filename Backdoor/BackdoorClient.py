524068d5dd2d66bc9c9550f9931013209b15c067
Mehdi Himmiche <mhimmiche@radford.edu>
2017-09-17 17:05:53 -0400

    Update to the BackdoorClient
    For now, it parses multiple files, working on making it parse one single document securely

--- Backdoor/BackdoorClient.py
+++ Backdoor/BackdoorClient.py
@@ -2,7 +2,13 @@ import sys
 import defusedxml.ElementTree as ET
 
 def uploadAdmin(adminRoot):
+	# For now it just prints the values, will add SQL query later.
 	for child in adminRoot:
+		print child.tag, "With value ", child.text
+
+def uploadDoctor(doctorRoot):
+	# For now it just prints the values, will add SQL query later.
+	for child in doctorRoot:
 		print child.tag, "With value ", child.text
 
 def main():
@@ -16,6 +22,8 @@ def main():
 	print "root for XML: ", eRootValue
 	if eRootValue == "SystemAdministratorUserProfile":
 		uploadAdmin(eRoot)
+	elif eRootValue == "DoctorUserProfile":
+		uploadDoctor(eRoot)
 
 
 if __name__ == "__main__":
