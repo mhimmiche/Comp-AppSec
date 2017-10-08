5b7b843cfe5cc78bbccfb80851b95011c691206c
Mehdi Himmiche <mhimmiche@radford.edu>
2017-10-08 12:03:08 -0400

    Add files via upload

--- Backdoor/BackdoorClient.py
+++ Backdoor/BackdoorClient.py
@@ -2,18 +2,24 @@ import sys
 import defusedxml.ElementTree as ET
 
 def uploadAdmin(adminRoot):
-	# For now it just prints the values, will add SQL query later.
-	print "[*] Found an Administrator [*]"
+	print "[*] Found an Administrator"
+	vals = {"Username":False,"Roles":False,"Permissions":False,"FirstName":False,"LastName":False}	
 	for child in adminRoot:
-		print ">>>>> ", child.tag, "With value ", child.text
-	print "[*] End of Administrator [*]"
+		#print ">>>>> ", child.tag, "With value ", child.text
+		for k,v in vals.items():
+			if child.tag == k:
+				vals[k] = child.text
+	for k,v in vals.items():
+		print k, v
+	print "[*] End of Administrator"
 
 def uploadDoctor(doctorRoot):
-	# For now it just prints the values, will add SQL query later.
-	print "[*] Found a Doctor [*]"
+	print "[*] Found a Doctor"
 	for child in doctorRoot:
 		print ">>>>> ", child.tag, "With value ", child.text
-	print "[*] End of Doctor [*]"
+	print "[*] End of Doctor"
+
+
 
 def main():
 	# Printing out the provided
@@ -24,12 +30,17 @@ def main():
 	eRoot = e.getroot()
 	eRootValue = eRoot.tag
 	print "root for XML: ", eRootValue
-	for child in eRoot:
-		if child.tag == "SystemAdministratorUserProfile":			
-			uploadAdmin(child)			
-		elif child.tag == "DoctorUserProfile":			
-			uploadDoctor(child)
-			
+	if eRootValue == "DBFile":
+		for child in eRoot:
+			if child.tag == "SystemAdministratorUserProfile":			
+				uploadAdmin(child)			
+			#elif child.tag == "DoctorUserProfile":			
+				#uploadDoctor(child)
+	else:
+		print("[*] Wrong XML file provided.")
+		exit(0)
+
+
 
 
 if __name__ == "__main__":
